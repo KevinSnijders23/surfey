0a3e25d2dddf155d490cfe99088cf4af13b1a36d
Kevin Snijders <kevin_snijders@live.nl>
2024-08-25 17:34:16 +0200

    Updates

--- static/Refresh-CeIwDq27.js
+++
@@ -1,1 +0,0 @@
-import{z as t,A as r,p as o,r as c,i as n,F as i}from"./index-BiLvG-Bv.js";const p=()=>{const a=t(),s=r(),e=o();return c.useEffect(()=>{e("/"+a["*"]+s.search,{replace:!0})},[]),n(i,{})};export{p as default};
