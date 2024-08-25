2e354bae1640b01e450378735577729f0a1cfffb
Kevin Snijders <kevin_snijders@live.nl>
2024-08-25 17:19:33 +0200

    Updates

---
+++ static/403-CYGI3zNF.js
@@ -0,0 +1,1 @@
+import{r as s,q as r,p as o,i as t,l as i}from"./index-BiLvG-Bv.js";import{R as l}from"./index-CwO905tV.js";const u=s.memo(()=>{const a=r(),e=o();return t(l,{status:"403",title:"403",subTitle:a.formatMessage({id:"layout.error.403"}),extra:t(i,{type:"primary",onClick:()=>{e("/")},children:"Back Home"})})});export{u as default};
---
+++ static/index-DpaYOnrC.js
@@ -0,0 +1,1 @@
+import{p as e,q as a,i as s}from"./index-BiLvG-Bv.js";const r=()=>(e(),a(),s("div",{children:"Other page"}));export{r as default};
